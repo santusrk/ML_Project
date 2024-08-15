bb4658aa6a44b42ee587008a9c50c222c53366ab
santusrk <srikantnayak.2010@gmail.com>
2024-08-16 01:57:33 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,11 @@
 import sys
 import logging
+from src.logger import logging
+
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name= exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(exc_tb.tb_file_name,lineno,str(error))
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str( ))
 
     return error_message
 
@@ -16,3 +18,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__ =='__main__':
+    try:
+        acc=23/0
+    except Exception as e :
+        print(e)
+        logging.info("indeterminate form")
+        raise CustomException(e,sys)
